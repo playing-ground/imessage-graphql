b503167d4bdb73bb5f4e2d573df00205e1b81831
Mateus Abelli <mateusabelli@gmail.com>
2022-11-23 20:53:16 -0300

    Initialize backend

---
+++ backend/src/index.ts
@@ -0,0 +1,54 @@
+// npm install @apollo/server express graphql cors body-parser
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import http from "http";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+
+async function main() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+}
+
+main().catch((err) => console.log(err));
+
+// const app = express();
+// const httpServer = http.createServer(app);
+// const server = new ApolloServer<MyContext>({
+//   typeDefs,
+//   resolvers,
+//   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+// });
+// await server.start();
+// app.use(
+//   "/graphql",
+//   cors<cors.CorsRequest>(),
+//   json(),
+//   expressMiddleware(server, {
+//     context: async ({ req }) => ({ token: req.headers.token }),
+//   })
+// );
+
+// await new Promise<void>((resolve) =>
+//   httpServer.listen({ port: 4000 }, resolve)
+// );
+// console.log(`🚀 Server ready at http://localhost:4000/graphql`);
