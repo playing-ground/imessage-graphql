75bb97e054c9be42546fee137c8414634893a0cb
Mateus Abelli <mateusabelli@gmail.com>
2022-11-23 20:53:05 -0300

    Initialize frontend

---
+++ frontend/src/components/Auth/Auth.tsx
@@ -0,0 +1,57 @@
+import { Text, Button, Center, Stack, Image, Input } from "@chakra-ui/react";
+import { Session } from "next-auth";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+
+interface IAuthProps {
+  session: Session | null;
+  reloadSession: () => void;
+}
+
+const Auth: React.FC<IAuthProps> = ({ session, reloadSession }) => {
+  const [username, setUsername] = useState("");
+
+  const onSubmit = async () => {
+    try {
+      /**
+       * createUserName mutation to send our username to the GraphQL API
+       */
+    } catch (error) {
+      console.log("onSubmit error", error);
+    }
+  };
+
+  return (
+    <Center height="100vh">
+      <Stack spacing={8} align="center">
+        {session ? (
+          <>
+            <Text fontSize="3xl">Create a username</Text>
+            <Input
+              placeholder="Enter a username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            <Button width="100%" onClick={onSubmit}>
+              Save
+            </Button>
+          </>
+        ) : (
+          <>
+            <Text fontSize="3xl">MessengerQL</Text>
+            <Button
+              onClick={() => signIn("google")}
+              leftIcon={
+                <Image height="20px" src="/images/googlelogo.png" alt="" />
+              }
+            >
+              Continue with Google
+            </Button>
+          </>
+        )}
+      </Stack>
+    </Center>
+  );
+};
+
+export default Auth;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { Box } from "@chakra-ui/react";
+import { NextPageContext } from "next";
+import { getSession, useSession } from "next-auth/react";
+import Auth from "../components/Auth/Auth";
+import Chat from "../components/Chat/Chat";
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  console.log("HERE IS DATA", session);
+
+  const reloadSession = () => {};
+
+  return (
+    <Box>
+      {session?.user?.username ? (
+        <Chat />
+      ) : (
+        <Auth session={session} reloadSession={reloadSession} />
+      )}
+    </Box>
+  );
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
